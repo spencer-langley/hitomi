3de21062c57a3284e11196833ecb14174f967819
Spencer Langley <spencer.james.langley@gmail.com>
2017-06-06 19:50:18 -0500

    2 Factor Auth
    Use app specific password to see that fixes app harbor 500 on email send
    call

--- Hitomi/Controllers/HomeController.cs
+++ Hitomi/Controllers/HomeController.cs
@@ -55,22 +55,22 @@ namespace Hitomi.Controllers
         {
             var json = new JavaScriptSerializer().Serialize(session);
 
-            //MailMessage mailMessage = new MailMessage();
-            //mailMessage.IsBodyHtml = true;
-            //mailMessage.To.Add("datasender.2016@gmail.com");
-            //mailMessage.From = new MailAddress("datasender.2016@gmail.com");
-            //mailMessage.Body = json;
-            //mailMessage.Subject = session.VideoName + " " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.IsBodyHtml = true;
+            mailMessage.To.Add("datasender.2016@gmail.com");
+            mailMessage.From = new MailAddress("datasender.2016@gmail.com");
+            mailMessage.Body = json;
+            mailMessage.Subject = session.VideoName + " " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
 
-            //SmtpClient smtpClient = new SmtpClient();
-            //smtpClient.UseDefaultCredentials = false;
-            //smtpClient.Credentials = new System.Net.NetworkCredential("datasender.2016@gmail.com", "DataSender");
-            //smtpClient.Port = 587;
-            //smtpClient.Host = "smtp.gmail.com";
-            //smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            //smtpClient.EnableSsl = true;
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new System.Net.NetworkCredential("datasender.2016@gmail.com", "njihkpulboxbszsj");
+            smtpClient.Port = 587;
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
 
-            //smtpClient.Send(mailMessage);
+            smtpClient.Send(mailMessage);
 
             return Json("Message received and ignored");
         }
